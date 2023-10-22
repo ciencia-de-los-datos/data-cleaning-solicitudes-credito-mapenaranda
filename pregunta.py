d85ec612565fa9de9202c4dc06b5795c99ec3358
mapenaranda <mapenarandam@unal.edu.co>
2023-10-23 00:46:22 +0200

    arlanda1

--- pregunta.py
+++ pregunta.py
@@ -8,43 +8,19 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
 
-
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
 
-    #
-    #
-    
-    df= df[df.columns[1:]]
-    df.columns.values
-
-    df['sexo']= df['sexo'].str.lower() 
-
-    df['tipo_de_emprendimiento']= df['tipo_de_emprendimiento'].str.lower()
-
-    df['idea_negocio']= df['idea_negocio'].str.lower()
-    df['idea_negocio']=df['idea_negocio'].str.replace("-","_")
-    df['idea_negocio']=df['idea_negocio'].str.replace("_"," ")
-    df['idea_negocio']=df['idea_negocio'].str.strip()
-
-    df['barrio']= df['barrio'].str.lower()
-    df['barrio']=df['barrio'].str.replace("-","_")
-    df['barrio']=df['barrio'].str.replace("_"," ")
-
-    df['línea_credito']= df['línea_credito'].str.lower()
-    df['línea_credito']=df['línea_credito'].str.replace("-","_")
-    df['línea_credito']=df['línea_credito'].str.replace("_"," ")
-    df['línea_credito']=df['línea_credito'].str.replace(".","")
-    df['línea_credito']=df['línea_credito'].str.strip()
-
-    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio,dayfirst=True)
-
-    df['monto_del_credito']=df['monto_del_credito'].str.replace(",","")
-    df['monto_del_credito']=df['monto_del_credito'].str.replace("$","")
-    df['monto_del_credito']=df['monto_del_credito'].str.strip()
-    df['monto_del_credito'] = df['monto_del_credito'].apply(pd.to_numeric, downcast="integer", errors='ignore')
-    df.drop_duplicates(inplace=True)
     df.dropna(inplace=True)
+
+    df = df.apply(lambda x: x.astype(str).str.replace("_","-").str.replace("-"," ").str.replace("$","").str.replace(",",""))
+    df = df.apply(lambda x: x.astype(str).str.lower().str.strip(" "))
+    df.fecha_de_beneficio = pd.to_datetime(df["fecha_de_beneficio"],format='mixed', dayfirst = True)
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+    
+    df.drop_duplicates(inplace=True)
+
+    return df
     
     return df
