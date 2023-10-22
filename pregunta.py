9443fcd7aa1964338c11cb12d2ab6890011254e2
mapenaranda <mapenarandam@unal.edu.co>
2023-10-23 01:12:22 +0200

    juanpam

--- pregunta.py
+++ pregunta.py
@@ -9,25 +9,37 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 import pandas as pd
 
 def clean_data():
-    # Cargamos el archivo y eliminamos de columna innecesaria
-    df = pd.read_csv("solicitudes_credito.csv", sep=";").drop(columns=['Unnamed: 0'])
-    # Eliminamos duplicados y valores nulos
-    df = df.dropna().drop_duplicates()
+    
+    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
+    df.reset_index(inplace=True,drop=True)
+    
+    # Date check  
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'],dayfirst=True)
+    
+    df.dropna(axis='index',inplace=True)
+    df.drop_duplicates(inplace=True)
 
-    # Normalizamos las columnas de sexo, tipo de emprendimiento, idea de negocio y línea de crédito con una función lambda
-    normalize_str = lambda x: x.lower().replace('_', ' ').replace('-', ' ').strip()
-    df['sexo'] = df['sexo'].apply(normalize_str)
-    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].apply(normalize_str)
-    df['idea_negocio'] = df['idea_negocio'].apply(normalize_str)
-    df['línea_credito'] = df['línea_credito'].apply(normalize_str)
-    #Para la columna barrio reemplazamos _ por - y luego - por espacio porque hay valores que tienen _ y otros que tienen - y así quedan todos iguales
-    df['barrio'] = df['barrio'].apply(lambda x: x.lower().replace('_', '-').replace('-', ' '))
+    # Lowercase check
+    df['sexo'] = df['sexo'].str.lower().astype(str).str.strip()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower().astype(str)
+    df['idea_negocio'] = df['idea_negocio'].str.lower().astype(str)
+    df['barrio'] = df['barrio'].str.lower().astype(str)
+    df['línea_credito'] = df['línea_credito'].str.lower().astype(str)
+     
+    # Whitespaces, hyphen and underscore checks
+    df['idea_negocio'] = df['idea_negocio'].str.replace('_',' ').str.replace('-',' ').str.strip()
+    df['barrio'] = df['barrio'].str.replace('_','-').str.replace('-',' ')
+    df['línea_credito'] = df['línea_credito'].str.replace('_',' ').str.replace('-',' ').str.strip()
 
-    # Convertimos monto del credito a número
-    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',', '').str.replace('$', '', regex=False).str.replace(' ', '').astype(float)
-    # Cambiamos el formato de la fecha de la columna fecha_de_beneficio
-    #df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True)
-    # Eliminamos duplicados y valores nulos
-    df = df.drop_duplicates().dropna()
+    # Money check
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',','').str.replace('$','',regex=False).str.replace(' ','').str.strip().astype(float)  
+     
+    # Others
+    # df['línea_credito'] = df['línea_credito'].str.replace('soli diaria','solidaria')
+    # df['idea_negocio'] = df['idea_negocio'].str.replace('[^a-zA-Z0-9 \n\.]', ' ',regex=True).str.replace('organizaci n y','organizacion y').str.replace('pa alera','panalera')
+    # df['barrio'] =  df['barrio'].str.replace('[^a-zA-Z0-9 \n\.]',' ',regex=True).str.replace('bel n','belen').str.replace('san jos  de la monta a','san jose de la montana').str.replace('antonio nari  o','antonio narino').str.replace('antonio nari o','antonio narino')
+
+    df.drop_duplicates(inplace=True)
+    df.dropna(axis='index',inplace=True)
 
     return df
