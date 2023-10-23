271f6d100745e6c10ac4821058a79e8308d6bc3e
mapenaranda <mapenarandam@unal.edu.co>
2023-10-23 06:34:35 +0200

    ya

--- pregunta.py
+++ pregunta.py
@@ -11,35 +11,14 @@ import re
 
 def clean_data():
     df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
-    df.reset_index(inplace=True,drop=True)
-    
-    # Date check  
-    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], format='mixed', dayfirst=True)
-    
-    df.dropna(axis='index',inplace=True)
-    df.drop_duplicates(inplace=True)
 
-    # Lowercase check
-    df['sexo'] = df['sexo'].str.lower().astype(str).str.strip()
-    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower().astype(str)
-    df['idea_negocio'] = df['idea_negocio'].str.lower().astype(str)
-    df['barrio'] = df['barrio'].str.lower().astype(str)
-    df['línea_credito'] = df['línea_credito'].str.lower().astype(str)
-     
-    # Whitespaces, hyphen and underscore checks
-    df['idea_negocio'] = df['idea_negocio'].str.replace('_',' ').str.replace('-',' ').str.strip()
-    df['barrio'] = df['barrio'].str.replace('_','-').str.replace('-',' ')
-    df['línea_credito'] = df['línea_credito'].str.replace('_',' ').str.replace('-',' ').str.strip()
+    df.dropna(inplace=True)
 
-    # Money check
-    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',','').str.replace('$','',regex=False).str.replace(' ','').str.strip().astype(float)  
-     
-    # Others
-    # df['línea_credito'] = df['línea_credito'].str.replace('soli diaria','solidaria')
-    # df['idea_negocio'] = df['idea_negocio'].str.replace('[^a-zA-Z0-9 \n\.]', ' ',regex=True).str.replace('organizaci n y','organizacion y').str.replace('pa alera','panalera')
-    # df['barrio'] =  df['barrio'].str.replace('[^a-zA-Z0-9 \n\.]',' ',regex=True).str.replace('bel n','belen').str.replace('san jos  de la monta a','san jose de la montana').str.replace('antonio nari  o','antonio narino').str.replace('antonio nari o','antonio narino')
+    df = df.apply(lambda x: x.astype(str).str.replace("_","-").str.replace("-"," ").str.replace("$","").str.replace(",",""))
+    df = df.apply(lambda x: x.astype(str).str.lower().str.strip(" "))
+    df.fecha_de_beneficio = pd.to_datetime(df["fecha_de_beneficio"],format='mixed', dayfirst = True)
+    df.monto_del_credito = df.monto_del_credito.astype(float)
 
     df.drop_duplicates(inplace=True)
-    df.dropna(axis='index',inplace=True)
     
     return df
